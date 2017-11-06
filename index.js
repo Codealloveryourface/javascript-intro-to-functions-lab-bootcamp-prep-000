c51bebaaa5a645a564a9f8c32dc876f5ef3b900c
Michal <mggrupa@gmail.com>
2017-11-06 11:55:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,6 +14,6 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-function sayHiToGrandma(hello) {
+function sayHiToGrandma(string) {
   return string.toLowerCase("I can't hear you")
 }

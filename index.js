929e3b4881c5cf86ba523be9d4aeae789865a1f5
Michal <mggrupa@gmail.com>
2017-11-06 13:38:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,6 +21,6 @@ function sayHiToGrandma(string) {
     if (string.toUpperCase() === string) {
     return "YES INDEED!"
       }
-      if (string ("I love you, Grandma.") === string) {
+      if ("I love you, Grandma." === string) {
     return "I love you too."
       }
